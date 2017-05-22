09e7715e7629e3af2b41b4a08cab40343d894699
Nhut.Vo <nhut.vo@dou.com.vn>
2017-05-22 17:31:03 +0700

    bunder js

--- client/src/app/task/task.component.ts
+++ client/src/app/task/task.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent implements OnInit , AfterViewInit{
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    $("#todo, #inprogress, #inprogress1, #completed").sortable({
+                connectWith: ".connectList",
+                update: function( event, ui ) {
+
+                    var todo = $( "#todo" ).sortable( "toArray" );
+                    var inprogress = $( "#inprogress" ).sortable( "toArray" );
+                    var completed = $( "#completed" ).sortable( "toArray" );
+                    //console.log("ToDo: " + window.JSON.stringify(todo) + " --- " + "In Progress: " + window.JSON.stringify(inprogress) + " --- " + "Completed: " + window.JSON.stringify(completed));
+                }
+            }).disableSelection();
   }
 
 }
--- client/src/app/task2/task2.component.ts
+++ client/src/app/task2/task2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -6,17 +6,60 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './task2.component.html',
   styleUrls: ['./task2.component.css']
 })
-export class Task2Component implements OnInit {
-  isEdit = false;
-  name = 'nhut vo'
-  constructor() { }
+export class Task2Component implements OnInit, AfterViewInit {
 
   ngOnInit() {
   }
+  private availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala"
+    ];
+  constructor(){
+    // $( "#tags" ).autocomplete({
+    //   source: this.availableTags
+    // });
+  }
 
-  bindingName(newValue):void{
-    this.name = newValue;
-    console.log(newValue)
+  ngAfterViewInit(){
+
+     $( "#tags" ).autocomplete({
+      source: this.availableTags
+    });
+
+    // Applied globally on all textareas with the "autoExpand" class
+    $(document).one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    }).on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows - 1;
+    });
+
+    
+    autosize($('#textDemo'));
   }
 
 }
